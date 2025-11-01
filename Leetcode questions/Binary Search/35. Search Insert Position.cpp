f7eeffdf14dc36bb97f021819b20bb29ed6f03e1
AryanSuwalka <aryansuwalka302@gmail.com>
2025-11-01 23:49:51 +0530

    binary search question

---
+++ Leetcode questions/Binary Search/35. Search Insert Position.cpp
@@ -0,0 +1,24 @@
+// 35. Search Insert Position
+
+int searchInsert(vector<int>& nums, int target) {
+    int s = 0;
+    int e = nums.size() - 1;
+    int mid = s + (e - s) / 2;
+    int ans = 0;
+
+    if (target > nums[e]) {
+        return e + 1;
+    }
+
+    while (s <= e) {
+        if (nums[mid] >= target) {
+            ans = mid;
+            e = mid - 1;
+        } else {
+            s = mid + 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+
